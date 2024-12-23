51e34900929e79af33e74c28083f17ce27ceb49e
Alexandr Kabanau <azraill1989@gmail.com>
2024-12-23 15:31:55 +0300

    add: add authorization

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service'
+import { Injectable } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { AuthDto } from './dto/auth.dto'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+	constructor(
+		private jwt: JwtService,
+		private userService: UserService
+	) {}
+
+	async login(dto: AuthDto) {
+		return dto
+	}
+}
