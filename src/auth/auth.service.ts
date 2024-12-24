1c28572cfdd2b999be6efd4c82c0374f5c0149d9
Alexandr Kabanau <azraill1989@gmail.com>
2024-12-24 11:14:18 +0300

    feat: service authority

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,13 @@
 import { UserService } from 'src/user/user.service'
-import { Injectable } from '@nestjs/common'
+import {
+	BadRequestException,
+	Injectable,
+	NotFoundException,
+	UnauthorizedException
+} from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
 import { AuthDto } from './dto/auth.dto'
+import { verify } from 'argon2'
 
 @Injectable()
 export class AuthService {
@@ -11,6 +17,51 @@ export class AuthService {
 	) {}
 
 	async login(dto: AuthDto) {
-		return dto
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		const { password, ...user } = await this.validateUser(dto)
+
+		const tokens = this.issueTokens(user.id)
+		return {
+			user,
+			...tokens
+		}
+	}
+	async register(dto: AuthDto) {
+		const oldUser = await this.userService.getByEmail(dto.email)
+
+		if (oldUser) throw new BadRequestException('User alredy exist')
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		const { password, ...user } = await this.userService.create(dto)
+
+		const tokens = this.issueTokens(user.id)
+		return {
+			user,
+			...tokens
+		}
+	}
+
+	private issueTokens(userId: string) {
+		const data = { id: userId }
+
+		const accessToken = this.jwt.sign(data, {
+			expiresIn: '1h'
+		})
+		const refreshToken = this.jwt.sign(data, {
+			expiresIn: '7d'
+		})
+
+		return { accessToken, refreshToken }
+	}
+
+	private async validateUser(dto: AuthDto) {
+		const user = await this.userService.getByEmail(dto.email)
+
+		if (!user) throw new NotFoundException('User not found')
+
+		const isValidate = await verify(user.password, dto.password)
+
+		if (!isValidate) throw new UnauthorizedException('Invalid password')
+
+		return user
 	}
 }
