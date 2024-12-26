b6c14d11ae95403622537ee8d55e01024f10e32b
Alexandr Kabanau <azraill1989@gmail.com>
2024-12-26 14:04:29 +0300

    fix: add TaskModule into app modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
 import { AuthModule } from './auth/auth.module'
 import { UserModule } from './user/user.module'
 import { ConfigModule } from '@nestjs/config'
+import { TaskModule } from './task/task.module'
 
 @Module({
-	imports: [ConfigModule.forRoot(), AuthModule, UserModule]
+	imports: [ConfigModule.forRoot(), AuthModule, UserModule, TaskModule]
 })
 export class AppModule {}
