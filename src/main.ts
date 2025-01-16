4e53f15bb5756ac6b32fcfc0bc3a29837b507d2a
Alexandr Kabanau <azraill1989@gmail.com>
2025-01-16 14:41:28 +0300

    fixcommit

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,8 @@ const port = process.env.PORT || 4200;
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 
-	app.setGlobalPrefix('api')
-	app.use(cookieParser())
+	// app.setGlobalPrefix('api')
+	// app.use(cookieParser())
 	// app.enableCors({
 	// 	origin: ['http://localhost:3000'],
 	// 	credentials: true,
