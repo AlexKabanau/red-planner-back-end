f0f1a3369a3ec192dc9a981b86abfdd8eb5add77
Alexandr Kabanau <azraill1989@gmail.com>
2025-01-16 14:57:17 +0300

    fixcommit2

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,14 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import * as cookieParser from 'cookie-parser'
 
-const port = process.env.PORT || 4200;
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+
+
 
 async function bootstrap() {
+	const port = process.env.PORT || 4200;
 	const app = await NestFactory.create(AppModule)
 
 	// app.setGlobalPrefix('api')
