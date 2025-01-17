bdd14ab92ad3e3e810cb5772374ca09394938d4b
Alexandr Kabanau <azraill1989@gmail.com>
2025-01-17 13:25:03 +0300

    return commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common'
 import { AuthModule } from './auth/auth.module'
 import { UserModule } from './user/user.module'
-// import { ConfigModule } from '@nestjs/config'
+import { ConfigModule } from '@nestjs/config'
 import { TaskModule } from './task/task.module'
 import { TimeBlockModule } from './time-block/time-block.module'
 import { PomodoroModule } from './pomodoro/pomodoro.module'
 
 @Module({
 	imports: [
-		// ConfigModule.forRoot(),
+		ConfigModule.forRoot(),
 		AuthModule,
 		UserModule,
 		TaskModule,
